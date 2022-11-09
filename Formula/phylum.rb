1211dae0efb46999e0bd4d3ed59e9077eb0eabe8
Kyle Willmon <616067+kylewillmon@users.noreply.github.com>
2022-11-09 20:03:05 +0000

    phylum: add 3.12.1 bottle.

--- Formula/phylum.rb
+++ Formula/phylum.rb
@@ -6,6 +6,12 @@ class Phylum < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/phylum-dev/cli.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/phylum-dev/homebrew-cli/releases/download/phylum-3.12.1"
+    sha256 cellar: :any_skip_relocation, monterey:     "fd03416447a582420387542e23a1c1a965c245e282a053c139e4ce22ed4d0337"
+    sha256                               x86_64_linux: "e0e316b6ca6cce71ddd8bc84bc576e741a552cf7636d72b09da454bc5f1bac6a"
+  end
+
   depends_on "rust" => :build
 
   def install
