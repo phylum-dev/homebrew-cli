741324b06428a1bcf09539697894319bee7763f8
Kyle Willmon <kylewillmon@gmail.com>
2022-12-06 13:06:55 -0600

    phylum v4.0.1

--- Formula/phylum.rb
+++ Formula/phylum.rb
@@ -1,10 +1,9 @@
 class Phylum < Formula
   desc "Software Supply Chain Security CLI"
   homepage "https://phylum.io"
-  url "https://github.com/phylum-dev/cli/archive/refs/tags/v4.0.0.tar.gz"
-  sha256 "0e11dd83cae44fe180691c681acfe4e783c9130eda79b89af0efb4ce8887c514"
+  url "https://github.com/phylum-dev/cli/archive/refs/tags/v4.0.1.tar.gz"
+  sha256 "2167803fa383c48df818d739da7366fba022182dd3e20169272c24c2a863dfe5"
   license "GPL-3.0-or-later"
-  revision 1
   head "https://github.com/phylum-dev/cli.git", branch: "main"
 
   bottle do
