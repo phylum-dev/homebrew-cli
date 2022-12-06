c53244c1c67bbadc1b9b04a8c9d9cf9ee2e5006e
Kyle Willmon <616067+kylewillmon@users.noreply.github.com>
2022-12-06 19:50:10 +0000

    phylum: update 4.0.1 bottle.

--- Formula/phylum.rb
+++ Formula/phylum.rb
@@ -7,10 +7,10 @@ class Phylum < Formula
   head "https://github.com/phylum-dev/cli.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/phylum-dev/homebrew-cli/releases/download/phylum-4.0.0_1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "8754ae58126931e50deb0271e567fa8047e71f45a1043ec479146cc1d23dbd75"
-    sha256 cellar: :any_skip_relocation, monterey:       "be3ab2f1edd73028c601aea86619e4860d69d85355cbdbf44502dd80c10de289"
-    sha256                               x86_64_linux:   "5b669ce7d483185f46df4787ba64e816ccdbf0a99fa00b2efdbfa031d5dfd047"
+    root_url "https://github.com/phylum-dev/homebrew-cli/releases/download/phylum-4.0.1"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "015f211bf3d8bbd921f0bd74757e9a24fc6aefbb0988a6246306635b994d707d"
+    sha256 cellar: :any_skip_relocation, monterey:       "86b0e24441c091a9a1f3367555cd467ec6d5e1aeca3c2c75c82a30c84a669518"
+    sha256                               x86_64_linux:   "19bb1f74b38e0ad4b0d99c200dcf7ad06a855e2f6816d73b1cdb4f72d74dbc09"
   end
 
   depends_on "rust" => :build
