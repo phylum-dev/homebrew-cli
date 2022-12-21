51042e31c3981268d002f353ac75165b32e16db7
Kyle Willmon <kyle@phylum.io>
2022-12-21 12:20:48 -0600

    phylum v4.1.0 (#12)
    * phylum v4.1.0
    * phylum: update 4.1.0 bottle.
    
    Co-authored-by: Kyle Willmon <616067+kylewillmon@users.noreply.github.com>

--- Formula/phylum.rb
+++ Formula/phylum.rb
@@ -1,16 +1,16 @@
 class Phylum < Formula
   desc "Software Supply Chain Security CLI"
   homepage "https://phylum.io"
-  url "https://github.com/phylum-dev/cli/archive/refs/tags/v4.0.1.tar.gz"
-  sha256 "2167803fa383c48df818d739da7366fba022182dd3e20169272c24c2a863dfe5"
+  url "https://github.com/phylum-dev/cli/archive/refs/tags/v4.1.0.tar.gz"
+  sha256 "854ef465566930c24c60b29ffa9b6626f2da1ae3c902b645e7e836fe5a939337"
   license "GPL-3.0-or-later"
   head "https://github.com/phylum-dev/cli.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/phylum-dev/homebrew-cli/releases/download/phylum-4.0.1"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "015f211bf3d8bbd921f0bd74757e9a24fc6aefbb0988a6246306635b994d707d"
-    sha256 cellar: :any_skip_relocation, monterey:       "86b0e24441c091a9a1f3367555cd467ec6d5e1aeca3c2c75c82a30c84a669518"
-    sha256                               x86_64_linux:   "19bb1f74b38e0ad4b0d99c200dcf7ad06a855e2f6816d73b1cdb4f72d74dbc09"
+    root_url "https://github.com/phylum-dev/homebrew-cli/releases/download/phylum-4.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "452280a6a48ab5e1d188f3db923b46d9bfb5b4b21cb437499e15546572ade5b0"
+    sha256 cellar: :any_skip_relocation, monterey:       "e3082ff90ff1090ee29da42afabf1db39209f1dfa086d475ef52d25c8c031bb6"
+    sha256                               x86_64_linux:   "42443828356747a0fd58a58339c7fd3f0709d290d65853bf1f6a317a0b06afd7"
   end
 
   depends_on "rust" => :build
